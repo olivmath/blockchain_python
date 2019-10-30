d63adb2d291f3ec621e5d1cb31f8a9b41c5f7c3c
Lucas Oliveira <lucasbispodeoliveira@outlook.com>
2019-10-29 22:27:37 -0300

    adicionando 3 blocos

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ from blockchain import Blockchain
 if __name__ == '__main__':
     blockchain = Blockchain()
 
-    
+    blockchain.add_new_block('Primeiro Bloco!')
+    blockchain.add_new_block('Segundo Bloco!')
+    blockchain.add_new_block('terceiro Bloco!')
+
+    print(blockchain.get_all())
